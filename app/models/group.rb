0bac5b74ba286c8df033ce27d6fdf1a11c914058
NoelLincoln <noelsobryant@gmail.com>
2024-01-31 14:55:22 +0300

    add model validations and add associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
+# app/models/group.rb
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :user_transactions
+  validates :name, :icon, :created_at, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# app/models/user.rb
 class User < ApplicationRecord
+  has_many :user_transactions, foreign_key: :author_id
+  has_many :groups
+  validates :name, presence: true, uniqueness: true
 end
