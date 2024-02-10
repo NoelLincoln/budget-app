4407e588b6ca7906d8f4200dafe5787542a803e9
Noel Bryant <noelsobryant@gmail.com>
2024-02-10 10:03:18 +0300

    fix linter errors

--- spec/features/categories/index_spec.rb
+++ spec/features/categories/index_spec.rb
@@ -2,84 +2,85 @@ require 'rails_helper'
 
 RSpec.feature 'Categories Index Page', type: :feature do
   scenario 'User can see category names and total amounts' do
-    # Create a user
-    user = create(:user)
-
-    user.confirm
-
-    # Log in as the user
-    sign_in(user)
-
-    # Create some categories associated with the user
-    category1 = create(:category, name: 'Category 1', user:,
-                                  icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test.png'), 'image/png'))
-    category2 = create(:category, name: 'Category 2', user:,
-                                  icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test.png'), 'image/png'))
-
-    # Create user transactions for each category
-    create(:user_transaction, name: 'Transaction 1', amount: 100, category: category1, author: user)
-    create(:user_transaction, name: 'Transaction 2', amount: 200, category: category1, author: user)
-    create(:user_transaction, name: 'Transaction 3', amount: 150, category: category2, author: user)
-
-    # Visit the categories index page
-    visit categories_path
-
-    # Assert that the page contains the category names and total amounts
-    expect(page).to have_content('Category 1')
-    expect(page).to have_content('$300')
-    expect(page).to have_content('Category 2')
-    expect(page).to have_content('$150')
+    user = create_and_sign_in_user
+    categories = [
+      { name: 'Category 1',
+        transactions: [{ name: 'Transaction 1', amount: 100 }, { name: 'Transaction 2', amount: 200 }] },
+      { name: 'Category 2', transactions: [{ name: 'Transaction 3', amount: 150 }] }
+    ]
+    categories.each { |category_data| create_category_with_transactions(category_data, user) }
+    expect_category_names_and_total_amounts(categories)
   end
 
   scenario 'User can click on a category to view its transactions' do
-    # Create a user
-    user = create(:user)
-
-    # Confirm the user's account (simulate email confirmation)
-    user.confirm
-
-    # Log in as the user
-    login_as(user, scope: :user)
-
-    # Create a category associated with the user
-    category = create(:category, name: 'Category 1', user:,
-                                 icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test.png'), 'image/png'))
-
-    # Visit the categories index page
-    visit categories_path
-
-    # Debugging: Print the actual path
-    puts current_path
-
-    # Add authorization check
-    # Assuming you have an Ability class (usually in ability.rb) that defines permissions
-    # for reading transactions, you can mock the authorize! method like this:
-    allow_any_instance_of(Ability).to receive(:authorize!).with(:read, UserTransaction)
-
-    # Click on the entire card (including link)
-    # find('.card').click
-    click_link 'Category 1'
-
-    # Assert that the user is redirected to the category's transactions page
-    expect(page).to have_current_path(category_user_transactions_path(category))
+    user = create_and_sign_in_user
+    category = create_category_for_user(user)
+    visit_categories_index_page
+    click_category_link(category)
+    expect_redirected_to_category_transactions_page(category)
   end
 
   scenario 'User can add a new category' do
-    # Create a user
+    create_and_sign_in_user
+    visit_categories_index_page
+    click_add_new_category_link
+    expect_redirected_to_new_category_page
+  end
+
+  private
+
+  def create_and_sign_in_user
     user = create(:user)
-
     user.confirm
+    sign_in(user)
+    user
+  end
 
-    # Log in as the user
-    login_as(user, scope: :user)
+  def create_category_with_transactions(category_data, user)
+    category = create(:category, name: category_data[:name], user:,
+                                 icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test.png'),
+                                                           'image/png'))
+    calculate_total_amount(category_data[:transactions])
+    category_data[:transactions].each do |transaction_data|
+      create(:user_transaction, name: transaction_data[:name], amount: transaction_data[:amount], category:,
+                                author: user)
+    end
+    category
+  end
 
-    # Visit the categories index page
+  def expect_category_names_and_total_amounts(categories)
+    categories.each do |category_data|
+      expect(page).to have_content(category_data[:name])
+      expect(page).to have_content("$#{calculate_total_amount(category_data[:transactions])}")
+    end
+  end
+
+  def create_category_for_user(user)
+    create(:category, name: 'Category 1', user:,
+                      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test.png'), 'image/png'))
+  end
+
+  def visit_categories_index_page
     visit categories_path
+  end
 
-    # Click on the "Add a new category" link
+  def click_category_link(category)
+    click_link category.name
+  end
+
+  def expect_redirected_to_category_transactions_page(category)
+    expect(page).to have_current_path(category_user_transactions_path(category))
+  end
+
+  def click_add_new_category_link
     click_link 'add a new category'
+  end
 
-    # Assert that the user is redirected to the new category page
+  def expect_redirected_to_new_category_page
     expect(page).to have_current_path(new_category_path)
+  end
+
+  def calculate_total_amount(transactions)
+    transactions.sum { |t| t[:amount] }
   end
 end
